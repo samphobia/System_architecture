73d538494c33b1928fb8e715b0fc42b440d71fe9
Samuel Osinubi <samobafemisayo@gmail.com>
2023-03-08 15:44:30 +0100

    initialized server

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -1,3 +1,9 @@
 import jwt, datetime, os
 from flask import Flask, request
-from flask_mysqldb import MYSQL
+from flask_mysqldb import MySQL
+
+server = Flask(__name__)
+mysql = MySQL(server)
+
+server.config["MYSQL_HOSt"] = os.environ.get("MYSQL_HOST")
+print(server.config["MYSQL_HOST"])
